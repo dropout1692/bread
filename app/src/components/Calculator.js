634e511bfbf62cfef93a4d3190b1e6999322bf41
Michal Marusinec <michal.marusinec@scr.sk>
2023-06-18 11:58:15 +0200

    baker's percentages bugfix

--- app/src/components/Calculator.js
+++ app/src/components/Calculator.js
@@ -1,110 +1,84 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function Calculator() {
 
-  const [inputs, setInputs] = useState({
-    weight: 0,
-    flour: 0,
-    water: 0,
-    levain: 0,
-    salt: 0,
-    other: 0
-  });
+  const [flour, setFlour] = useState(0);
+  const [water, setWater] = useState(0);
+  const [levain, setLevain] = useState(0);
+  const [salt, setSalt] = useState(0);
+  const [other, setOther] = useState(0);
 
-  const [coefficient, setCoefficient] = useState(0);
-
-  const [values, setValues] = useState({
-    flour: 0,
-    water: 0,
-    levain: 0,
-    salt: 0,
-    other: 0
-  });
-
-  const update = (el) => {
-    let name = el.target.name;
-    setInputs({...inputs, [name]: el.target.value});
+  const get = (name) => {
+    let value = parseInt(document.getElementsByName(name)[0].value);
+    return (isNaN(value) ? 0 : value);
   }
 
-  useEffect(() => {
-    let allIngredients =
-        parseInt(inputs.flour) +
-        parseInt(inputs.water) +
-        parseInt(inputs.levain) +
-        parseInt(inputs.salt) +
-        parseInt(inputs.other);
+  const round = (number) => {
+    return Math.round(number * 100) / 100;
+  }
 
-    console.log(`All: ${allIngredients}`)
-    console.log(`Weight: ${inputs.weight}`)
+  const calculate = () => {
 
-    setCoefficient(inputs.weight / allIngredients);
-    setValues({
-      ...values,
-      flour: inputs.flour * coefficient,
-      water: inputs.water * coefficient,
-      levain: inputs.levain * coefficient,
-      salt: inputs.salt * coefficient,
-      other: inputs.other * coefficient
-    });
-  }, [inputs]);
+    let _flour = get("flour");
+    let _water = get("water");
+    let _levain = get("levain");
+    let _salt = get("salt");
+    let _other = get("other");
 
-  const round = (num) => {
-    return Math.round(num * 100) / 100;
+    if(_flour === 0 || _water === 0){
+      return;
+    }
+
+    setFlour(_flour);
+    setWater(_water / 100 * _flour);
+    setLevain(_levain / 100 * _flour);
+    setSalt(_salt / 100 * _flour);
+    setOther(_other / 100 * _flour);
   }
 
   return <>
+    <form></form>
     <div id='calculator-frame'>
       <div id='results'>
         <div>
-          Coefficient: <span id='coefficient'>{round(coefficient)}</span>
+          Flour: <span id='flour'>{round(flour)} g</span>
         </div>
         <div>
-          Flour: <span id='flour'>{round(values.flour)} g</span>
+          Water: <span id='water'>{round(water)} g</span>
         </div>
         <div>
-          Water: <span id='water'>{round(values.water)} g</span>
+          Levain: <span id='levain'>{round(levain)} g</span>
         </div>
         <div>
-          Levain: <span id='levain'>{round(values.levain)} g</span>
+          Salt: <span id='salt'>{round(salt)} g</span>
         </div>
         <div>
-          Salt: <span id='salt'>{round(values.salt)} g</span>
-        </div>
-        <div>
-          Other: <span id='other'>{round(values.other)} g</span>
+          Other: <span id='other'>{round(other)} g</span>
         </div>
       </div>
       <div id='inputs'>
-        <div>
-          <label htmlFor='weight'>Target weight:</label>
-          <input type='text' name='weight' maxLength='6'
-                 onInput={update}/>
-        </div>
-        <div>
-          <label htmlFor='flour'>Flour %:</label>
-          <input type='text' name='flour' maxLength='5'
-                 onInput={update}/>
-        </div>
-        <div>
-          <label htmlFor='water'>Water %:</label>
-          <input type='text' name='water' maxLength='5'
-                 onInput={update}/>
-        </div>
-        <div>
-          <label htmlFor='levain'>Levain %:</label>
-          <input type='text' name='levain' maxLength='5'
-                 onInput={update}/>
-        </div>
-        <div>
-          <label htmlFor='salt'>Salt %:</label>
-          <input type='text' name='salt' maxLength='5'
-                 onInput={update}/>
-        </div>
-        <div>
-          <label htmlFor='other'>Other %:</label>
-          <input type='text' name='other' maxLength='5'
-                 onInput={update}/>
-        </div>
+        <form onInput={calculate}>
+          <div>
+            <label htmlFor='flour'>Flour weight:</label>
+            <input type='text' name='flour' maxLength='5'/>
+          </div>
+          <div>
+            <label htmlFor='water'>Water %:</label>
+            <input type='text' name='water' maxLength='5'/>
+          </div>
+          <div>
+            <label htmlFor='levain'>Levain %:</label>
+            <input type='text' name='levain' maxLength='5'/>
+          </div>
+          <div>
+            <label htmlFor='salt'>Salt %:</label>
+            <input type='text' name='salt' maxLength='5'/>
+          </div>
+          <div>
+            <label htmlFor='other'>Other %:</label>
+            <input type='text' name='other' maxLength='5'/>
+          </div>
+        </form>
       </div>
     </div>
   </>
